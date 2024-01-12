881031149c4222733c6f8fc2b903bb643d225c84
nikeshsharma192002 <nk.sharma192002@gmail.com>
2024-01-12 17:27:43 +0530

    Restaurant-website

---
+++ Chapter 09 - Optimizing our App/Coding/Components/Footer.js
@@ -0,0 +1,24 @@
+// Footer component for footer section
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a
+        href="https://www.linkedin.com/in/nikesh-sharma-a8500a250/"
+        target="_blank"
+        title="Nikesh sharma's Linkedin Profile"
+      >
+        Nikesh sharma
+      </a>
+      <i className="fa-solid fa-copyright"></i>
+        {year}
+        <strong>
+          Food<span>Fire</span>
+        </strong>
+    </div>
+  );
+};
+
+export default Footer;
